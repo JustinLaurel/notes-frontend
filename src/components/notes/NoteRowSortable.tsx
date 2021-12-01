ca39c266dde10404fc66846f913b528619f84bc4
Spring <wengkeatw@gmail.com>
2021-12-01 16:06:41 +0800

    Converting note edit input to textarea

---
+++ src/components/notes/NoteRowSortable.tsx
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from 'react';
+import { useSortable } from '@dnd-kit/sortable';
+import { Note } from '../../types';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../../state/store';
+import { Box, Button, Center, FormControl, Textarea } from '@chakra-ui/react';
+import EditButton from './EditButton';
+import DeleteButton from './DeleteButton';
+import { stopEditMode } from '../../state/reducers/noteActionViews';
+import autosize from 'autosize';
+import { editNote } from '../../state/reducers/notes';
+
+const EditTextarea = ({ note }: { note: Note }) => {
+    const textareaStyle = {
+        height: "32px",
+        padding: "0px",
+        verticalAlign: "top",
+        overflow: 'hidden',
+    };
+
+
+    const dispatch = useDispatch();
+    const [edited, setEdited] = useState(note.content);
+
+    //When note is sorted, the note parameter changes, but edited maintains its 
+    // original note.content's value, before the sort 
+    //This line updates the value of edited every time the note is sorted
+    useEffect(() => setEdited(note.content), [note.content]);
+
+
+    const saveNote = (e: React.FormEvent) => {
+        e.preventDefault();
+        dispatch(stopEditMode());
+        if (edited === note.content) return;
+
+        const toSave: Note = {...note, content: edited};
+        dispatch(editNote(toSave));
+    };
+
+    return (
+        <FormControl as="form" onSubmit={saveNote}>
+            <Textarea 
+                as="textarea"
+                id={note._id}
+                style={textareaStyle}
+                autoFocus
+                autoComplete="off"
+                value={edited}
+                rows={1}
+                spellCheck={false}
+                onChange={(e) => setEdited(e.currentTarget.value)}
+                onBlur={saveNote}
+                >
+            </Textarea>
+            <Button style={{display: "none"}} />
+        </FormControl>
+    );
+};
+
+const NoteRowSortable = ({ note }: { note: Note }) => {
+    const {
+        setNodeRef,
+        attributes,
+        listeners,
+        transition,
+        transform,
+    } = useSortable({ id: note.position });
+
+    const sortableStyle = {
+        transition,
+        transform: transform
+            ? `translate3d(${transform?.x}px, ${transform?.y}px, 0)`
+            : undefined
+    };
+    
+    //Subscribes to when EditButton is pressed
+    const actionTracker = useSelector((state: RootState) => state.noteActionViews);
+    const isBeingEdited = actionTracker.isEditing && actionTracker.noteId === note._id;
+    
+    const content = (
+        <Box id={note._id}
+            ref={setNodeRef} 
+            whiteSpace="pre"
+            {...attributes} 
+            {...listeners} 
+            style={sortableStyle}>
+                {note.content}
+        </Box>
+    );
+
+    const contentNode = isBeingEdited
+        ? <EditTextarea note={note} />
+        : content;
+
+    useEffect(() => {
+        if (isBeingEdited) {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            autosize(document.querySelector('textarea')!);
+        }
+    }, [isBeingEdited]);
+
+    return (
+        <React.Fragment key={note._id}>
+            {contentNode}
+            <Center>
+                <EditButton note={note} isEditing={isBeingEdited} />
+            </Center>
+            <Center>
+                <DeleteButton note={note} />
+            </Center>
+        </React.Fragment>
+    );
+};
+
+export default NoteRowSortable;
--- src/components/notes/NotesList.tsx
+++ src/components/notes/NotesList.tsx
@@ -1,108 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../state/store';
 
 import { DndContext, closestCenter } from '@dnd-kit/core';
-import { useSortable } from '@dnd-kit/sortable';
 import { restrictToParentElement, restrictToVerticalAxis } from '@dnd-kit/modifiers'; 
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
 
 import { Note } from '../../types';
 import { baseMargins } from '../../utils/styles';
-import { Grid, Center, Box, Input, FormControl, Button } from '@chakra-ui/react';
+import { Grid } from '@chakra-ui/react';
 
-import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
-
-import DeleteButton from './DeleteButton';
-import { editNote, sortNote } from '../../state/reducers/notes';
-import EditButton from './EditButton';
-import SkeletonList from './SkeletonList';
-import { stopEditMode } from '../../state/reducers/noteActionViews';
+import { sortNote } from '../../state/reducers/notes';
 import { getStoredToken } from '../../state/reducers/login';
 
-const NoteRowSortable = ({ note }: { note: Note }) => {
-    const {
-        setNodeRef,
-        attributes,
-        listeners,
-        transition,
-        transform,
-    } = useSortable({ id: note.position });
-
-    const sortableStyle = {
-        transition,
-        transform: transform
-            ? `translate3d(${transform?.x}px, ${transform?.y}px, 0)`
-            : undefined
-    };
-    
-    const inputStyle = {
-        height: "32px",
-        padding: "0px",
-        verticalAlign: "top",
-    };
-
-    const actionTracker = useSelector((state: RootState) => state.noteActionViews);
-    const dispatch = useDispatch();
-    const isBeingEdited = actionTracker.isEditing && actionTracker.noteId === note._id;
-
-    const [edited, setEdited] = useState(note.content);
-
-    //When note is sorted, the note parameter supplied changes, but edited maintains its 
-    // original note.content's value before sorting 
-    //This useEffect updates edited's value every time the note is sorted
-    useEffect(() => setEdited(note.content), [note.content]);
-
-    const saveNote = (e: React.FormEvent) => {
-        e.preventDefault();
-
-        dispatch(stopEditMode());
-        if (edited === note.content) return;
-
-        const toSave: Note = {...note, content: edited};
-        dispatch(editNote(toSave));
-    };
-    const input = (
-        <FormControl as="form" onSubmit={saveNote}>
-            <Input id={note._id}
-                style={inputStyle}
-                autoFocus
-                autoComplete="off"
-                value={edited}
-                onChange={(e) => setEdited(e.currentTarget.value)}
-                onBlur={saveNote}
-                >
-            </Input>
-            <Button style={{display: "none"}} />
-        </FormControl>
-    );
-
-    const content = (
-        <Box id={note._id}
-            ref={setNodeRef} 
-            {...attributes} 
-            {...listeners} 
-            style={sortableStyle}>
-                {note.content}
-        </Box>
-    );
-
-    const contentNode = isBeingEdited
-        ? input
-        : content;
-
-    return (
-        <React.Fragment key={note._id}>
-            {contentNode}
-            <Center>
-                <EditButton note={note} isEditing={isBeingEdited} />
-            </Center>
-            <Center>
-                <DeleteButton note={note} />
-            </Center>
-        </React.Fragment>
-    );
-};
-
+import SkeletonList from './SkeletonList';
+import NoteRowSortable from './NoteRowSortable';
 
 const NotesList = () => {
     const notes: Note[] = useSelector((state: RootState) => state.notes);
@@ -129,9 +41,9 @@ const NotesList = () => {
         else return;
     };
 
-    const theList = notes.length > 0 || getStoredToken()
-        ? notes.map(note => <NoteRowSortable note={note} key={note.position} />)
-        : <SkeletonList rows={5} />;
+    const theList = getStoredToken() && notes.length === 0
+        ? <SkeletonList rows={5} />
+        : notes.map(note => <NoteRowSortable note={note} key={note.position} />);
 
     return (
         <Grid {...gridStyle} {...baseMargins} {...noteStyle}>
